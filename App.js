2a09d9c7fc1f5b40ae6c082c8368fe84ca62cae3
DwennK <Dwenn@live.fr>
2024-01-31 17:01:26 +0100

    add files

--- App.js
+++ App.js
@@ -1,7 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Configuration, OpenAIApi } from 'openai'; // Import these classes
+
+
 
 export default function App() {
+  const configuration = new Configuration ({
+    apiKey : process.env.OPEN_API_KEY,
+  });
+  
+  const openai = new OpenAIApi(configuration);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
